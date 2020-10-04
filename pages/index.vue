c94a8eefe3811a8fc0188ff5624458c83157f907
hayatouzu <kira.sri779@gmail.com>
2020-10-04 14:35:39 +0900

    frist commit

---
+++ pages/index.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <header class="header">
+    </header>
+
+    <section class="about" id="about">
+    </section>
+
+    <section class="works">
+    </section>
+
+    <section class="contact" id="contact">
+    </section>
+  </div>
+</template>
+
+<script>
+  export default {
+
+  }
+</script>
+
+<style lang="scss" scoped>
+.header {
+  height: 100vh;
+  background-color: rgba(25, 25, 120, 0.9);
+}
+
+.about {
+  height: 100vh;
+  background-color: #ffffff;
+}
+
+.works {
+  height: 100vh;
+  background-color: #383634;
+}
+
+.contact {
+  height: 100vh;
+  background-color: rgba(25, 25, 120, 0.9);
+}
+</style>
